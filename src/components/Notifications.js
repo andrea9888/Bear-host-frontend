76339b37f41b9558cd52c270c84ecb0dbb9b68e0
Filip Miladinovic <filippilif18@gmail.com>
2020-06-25 00:26:36 +0200

    login finished

---
+++ src/components/Notifications.js
@@ -0,0 +1,7 @@
+import React from "react";
+export const Notification = (props) => {
+    return(
+            <div className="notification">{props.message}</div>
+        )
+
+}
--- src/components/login_forms/SignIn.js
+++ src/components/login_forms/SignIn.js
@@ -8,47 +8,45 @@ class SignIn extends React.Component {
       this.props = props;
       //this.handleChange = this.handleChange.bind(this);
       this.state = {
-        usernameHolderStr: "",
-        passHolderStr: "",
-        wrongUsername: false,
-        wrongPass: false
+        username: "",
+        password: "",
+        keep: false
       };
     }
     
     
-    usernameHolder = (usernameHolderStr) => {
-    this.setState({ usernameHolderStr });
+    holder = (name, value) => {
+      this.setState({ [name]:  value});
     }
-    passHolder = (passHolderStr) => {
-      this.setState({ passHolderStr });
-      }
   
     handlePass = (pass) => {
-      if(pass.match(/[a-z]/ ) && pass.match(/[A-Z]/) && pass.match(/[0-9]/) && pass.length >= 8){
+      if(pass.length >= 8){
           return true;
       }
-      this.setState({ wrongPass: true});
+      this.props.setNotify("Prekratka lozinka!");
       return false;
   
     }
   
     handleUsername = () => {
-      if(this.state.usernameHolderStr.length >= 3){
+      if(this.state.username.length >= 3){
           return true;
       }
-      this.setState({ wrongUsername: true});
+      this.props.setNotify("Prekratko korisničko ime!");
       return false;
     }
   
     async loginCall(data){
       var res = await auth.login(data);
       if(res === "error"){
-        this.setState({ usernameHolderStr: ""});
-        this.setState({ passHolderStr: ""});
+        this.props.setNotify("Pogrešno korisničko ime ili lozinka!");
+        this.setState({ username: ""});
+        this.setState({ password: ""});
       }else{
         localStorage.setItem('bear-host-access', res.accesToken);      
         localStorage.setItem('bear-host-refresh', res.refreshToken);
         this.props.updateLogStatus(true);
+        this.props.keepMeLogedUpdate(this.state.keep);
         this.props.history.push("/");
       }
   
@@ -57,13 +55,11 @@ class SignIn extends React.Component {
     handleSubmit = (e) => {
       e.preventDefault();
       var data = {
-        'username': this.state.usernameHolderStr,
-        'password': this.state.passHolderStr
+        'username': this.state.username,
+        'password': this.state.password
       }
       if(this.handleUsername(data.username) && this.handlePass(data.password)){
         this.loginCall(data); 
-      }else{
-        console.log("nesto je pogresno");
       }
   
   
@@ -72,16 +68,16 @@ class SignIn extends React.Component {
         return(
             <form onSubmit={this.handleSubmit} className="sign-in">
                 <h2>SIGN IN</h2>
-                <input type="text" placeholder="Username" className="input-shape" name="username" onChange={e =>  this.usernameHolder(e.target.value)} value={this.state.usernameHolderStr}/>
-                <input type="password" placeholder="Password" className="input-shape" name="password" onChange={e =>  this.passHolder(e.target.value)} value={this.state.passHolderStr}/>
+                <input type="text" placeholder="Username" className="input-shape" name="username" onChange={e =>  this.holder(e.target.name, e.target.value)} value={this.state.username}/>
+                <input type="password" placeholder="Password" className="input-shape" name="password" onChange={e =>  this.holder(e.target.name, e.target.value)} value={this.state.password}/>
                 <div className="section-bellow">
                 <div className="section-checkbox">
-                <input type="checkbox" name="keep" id="keep" className="keep-checkbtn"/>
-                <label htmlFor="keep" className="keep-label">Keep me logged in</label>
+                <input type="checkbox" name="keep" id="keep" className="keep-checkbtn" onChange={e =>  this.holder(e.target.name, e.target.checked)} value={this.state.keep}/>
+                <label htmlFor="keep" className="keep-label">Zapamti me</label>
                 </div>
-                <a href="" /*AwdaAAAAAAAAAAAA*/>Forgot Password?</a>
+                <a href="" /*AwdaAAAAAAAAAAAA*/ className="fp-link">Zaboravili ste lozinku?</a>
                 </div>
-                <button type="submit" className="signin-btn" >SIGN IN</button>
+                <button type="submit" className="signin-btn" >Prijavi se</button>
             </form>
 
 
