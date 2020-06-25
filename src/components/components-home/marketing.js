dacb40d1b8b5e0208fe22b2f80365939d1f4a1e3
andrea9888 <50518294+andrea9888@users.noreply.github.com>
2020-06-25 15:34:57 +0200

    Icons changed for enterprise

---
+++ src/components/components-home/marketing.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Marketing extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>Marketing</div>
+         );
+    }
+}
+ 
+export default Marketing;
